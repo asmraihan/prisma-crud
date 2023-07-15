2b0ccc3e8e8bc8560049d272709281ea4c071996
asmraihan <asmraihanctg14@gmail.com>
2023-07-15 17:16:01 +0600

    root

---
+++ app/blog/add/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import React from 'react'
+import { Toaster } from 'react-hot-toast'
+
+const AddBlog = () => {
+    return (
+        <>
+            <Toaster />
+            <div className='w-full m-auto flex my-4'>
+                <div className='flex flex-col'></div>
+            </div>
+        </>
+    )
+}
+
+export default AddBlog
