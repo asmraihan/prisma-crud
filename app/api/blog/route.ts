6090e4521a47d8224c30ff540c5325615a460e7d
asmraihan <asmraihanctg14@gmail.com>
2023-07-15 12:58:06 +0600

    prisma client

---
+++ app/api/blog/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+
+export const GET = async (req : Request, res : NextResponse) => {
+    console.log("GET")
+}
+export const POST = async (req : Request, res : NextResponse) => {
+    console.log("POST")
+}
